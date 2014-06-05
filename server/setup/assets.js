667e0cdbd7faaff78fcfe0d8ef9080a87dd4c14c
Juan Placencia <juan.placencia.512@gmail.com>
2014-06-05 09:06:57 -0500

    Add more handling for multi projects on a server

--- server/setup/assets.js
+++ server/setup/assets.js
@@ -5,10 +5,22 @@ var express = require('express');
 var path = require('path');
 
 var rootPath = path.join(__dirname, '../..');
-var projects = process.env.project || Object.keys(config.entry);
+var projects = process.env.project
+  ? process.env.project.split(',')
+  : Object.keys(config.entry);
 var singleProject = projects.length === 1;
 var basePath = path.join(rootPath, 'build', singleProject ? projects[0] : '');
 
 module.exports = function(app) {
+  var rootPage;
+
   app.use(express.static(basePath));
+  if(!singleProject) {
+    rootPage = projects.map(function(project) {
+      return '<div><a href="/' + project + '">' + project + '</a></div>';
+    }).join('');
+    app.get('/', function(request, response) {
+      response.send(rootPage);
+    });
+  }
 };
